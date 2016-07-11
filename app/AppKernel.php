3f9012d3f9f8b3c34887398dc0a804ced4bd26b4
LysanderPL <podania.12@gmail.com>
2016-07-11 19:17:07 +0200

    Implementing FOS

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new AppBundle\AppBundle(),
-            new UserBundle\UserBundle(),
+            new \FOS\UserBundle\FOSUserBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
