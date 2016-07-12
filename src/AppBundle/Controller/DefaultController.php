3de8b062e93e8ba448b43eea9dd3c6c0fc00056b
LysanderPL <podania.12@gmail.com>
2016-07-12 20:22:42 +0200

    Implementing FOS Bundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,18 +2,21 @@
 
 namespace AppBundle\Controller;
 
+use Library\Utilities\Helpers\ControllerAbstract;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class DefaultController extends ControllerAbstract
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        var_dump($user = $this->get('security.token_storage')->getToken()->getUser());
+        var_dump($this->getUser()->getSalt());
         // replace this example code with whatever you need
         return $this->render('AppBundle/default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -9,17 +9,15 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="User")
  * @ORM\Entity(repositoryClass="AppBundle\Entity\Repository\UserRepository")
  * */
-class User
+class User extends BaseUser
 {
-
-    /** @ORM\Column(type="integer", length=1) */
-    protected $status;
 
     /**
      * @ORM\Id
@@ -27,18 +25,6 @@ class User
      * @ORM\Column(type="string", length=32, unique=true)
      */
     protected $id;
-
-    /** @ORM\Column(type="string", length=20, unique=true) */
-    protected $login;
-
-    /** @ORM\Column(type="string", length=20, unique=true) */
-    protected $email;
-
-    /** @ORM\Column(type="string") */
-    protected $password;
-
-    /** @ORM\Column(type="datetime") */
-    protected $date_add;
 
     /**
      * @param string $int
@@ -55,86 +41,5 @@ class User
     {
         return $this->id;
     }
-
-    /**
-     * @param int $int
-     */
-    public function setStatus(int $int)
-    {
-        $this->status = $int;
-    }
-
-    /**
-     * @return int
-     */
-    public function getStatus() : int
-    {
-        return $this->status;
-    }
-
-    /**
-     * @param string $login
-     */
-    public function setLogin(string $login)
-    {
-        $this->login = $login;
-    }
-
-    /**
-     * @return String
-     */
-    public function getLogin() : string
-    {
-        return $this->login;
-    }
-
-    /**
-     * @param String $password
-     */
-    public function setPassword(string $password)
-    {
-        $this->password = md5($password);
-    }
-
-    /**
-     * @return string
-     */
-    public function getPassword() : string
-    {
-        return $this->password;
-    }
-
-    /**
-     * @param string $email
-     */
-    public function setEmail(string $email)
-    {
-        $this->email = $email;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEmail() : string
-    {
-        return $this->email;
-    }
-
-    /**
-     * @param \DateTime $dateTime
-     */
-    public function setDateAdd(\DateTime $dateTime)
-    {
-        $this->date_add = $dateTime;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDateAdd() : string
-    {
-        return $this->date_add;
-    }
-
 
 }
