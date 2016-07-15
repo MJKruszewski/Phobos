57c90a823c6a570dfab85361d59ea1b8f8340b73
LysanderPL <podania.12@gmail.com>
2016-07-15 13:46:12 +0200

    cleaning things up

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,16 +10,15 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends ControllerAbstract
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/app", name="homepage")
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        var_dump($this->getUser()->getSalt());
-        // replace this example code with whatever you need
-        return $this->render('AppBundle/default/index.html.twig', [
+        return $this->render('@AppBundle/Resources/views/Default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'user_name' => $this->getUser()->getUsername()
         ]);
     }
 }
