a55dbe164de199f38d46bb0016e765cfdfdcd968
LysanderPL <podania.12@gmail.com>
2016-07-23 15:56:26 +0200

    setting up default planet for user

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,8 +3,9 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Repository\NewsRepository;
+use AppBundle\Entity\Repository\PlanetRepository;
 use AppBundle\Entity\Repository\ProjectProgressRepository;
-use LibraryBundle\Utilities\Helpers\ControllerAbstract;
+use AppBundle\Library\Utilities\Helpers\ControllerAbstract;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,9 +19,23 @@ class DefaultController extends ControllerAbstract
      */
     public function indexAction(Request $request)
     {
+        /**
+         * @var PlanetRepository $planetRepository
+         */
+        $planetRepository = $this->getDoctrine()->getRepository('AppBundle:Planet');
+
+        $createNewPlanet = false;
+
+        if (empty($planetRepository->findAllToArrayByOwnerId($this->getUser()->getId()))) {
+            $createNewPlanet = true;
+        } elseif ($this->getSession()->get('actual_planet', 0) <= 0) {
+            $this->getSession()->set('actual_planet', $this->getPlanetRepository()->getActiveUserCapitalPlanet($this->getUser()->getId())->getId());
+        }
+
         return $this->render('@AppBundle/Resources/views/Default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
-            'user_name' => $this->getUser()->getUsername()
+            'user_name' => $this->getUser()->getUsername(),
+            'create_new_planet' => $createNewPlanet
         ]);
     }
 
