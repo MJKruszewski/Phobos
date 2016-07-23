aefb2eee7c842accf5dc7e9350f5314bc8d875e2
LysanderPL <podania.12@gmail.com>
2016-07-23 10:55:49 +0200

    Added CLimate Directory entity

---
+++ src/AppBundle/Entity/ClimateDirectory.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maciej
+ * Date: 22.07.16
+ * Time: 22:39
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="ClimateDirectory")
+ * */
+class ClimateDirectory
+{
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(name="name", type="string", length=20 )
+     */
+    private $name;
+
+    /**
+     * @return int
+     */
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+    
+    
+}
