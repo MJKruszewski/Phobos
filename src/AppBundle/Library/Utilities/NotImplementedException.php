b2adbe52848a1800f7e1b81b3f547b4a5173881d
LysanderPL <podania.12@gmail.com>
2016-11-27 12:48:34 +0100

    new exception

---
+++ src/AppBundle/Library/Utilities/NotImplementedException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maciej
+ * Date: 13.11.16
+ * Time: 17:30
+ */
+
+namespace AppBundle\Library\Utilities;
+
+
+class NotImplementedException extends \Exception
+{
+
+}
